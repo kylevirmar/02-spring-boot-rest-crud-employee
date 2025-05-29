7e2715c60bdd0299d00c1c7c00233bf33ed699c9
Kyle Virmar Millendez <kylevirmarmillendez@Kyles-MacBook-Air.local>
2025-05-29 16:41:26 +0800

    feature1

--- src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CruddemoApplication {
 	//testing
+	//feature1
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
